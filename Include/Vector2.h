7e921877aad266220331c8a96d84a2fc3a7418b2
Liav Zidkiyaho <liavz2909@gmail.com>
2025-04-05 15:12:59 +0300

    Created Vector2 class

---
+++ Include/Vector2.h
@@ -0,0 +1,40 @@
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+#include <cmath>
+#include <iostream>
+
+class Vector2
+{
+private:
+    float x;
+    float y;
+
+public:
+    // Constructors
+    Vector2();
+    Vector2(float x, float y);
+
+    // Operators
+    Vector2 operator+(const Vector2& other) const;
+    Vector2 operator-(const Vector2& other) const;
+    Vector2 operator*(float scalar) const;
+    Vector2 operator/(float scalar) const;
+
+    // Methods
+    static float Length(const Vector2& v);
+    static float Distance(const Vector2& a, const Vector2& b);
+    static float Dot(const Vector2& a, const Vector2& b);
+    static float Cross(const Vector2& a, const Vector2& b);
+    Vector2 Normalize() const;
+
+    // Getters
+    float GetX() const;
+    float GetY() const;
+
+    // Setters
+    void SetX(float x);
+    void SetY(float y);
+};
+
+#endif //VECTOR2_H
---
+++ Source/Vector2.cpp
@@ -0,0 +1,63 @@
+#include "../Include/Vector2.h"
+
+// Constructors
+Vector2::Vector2() : x(0.0f), y(0.0f) {}
+
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+
+// Operators
+Vector2 Vector2::operator+(const Vector2& other) const {
+    return Vector2(x + other.x, y + other.y);
+}
+
+Vector2 Vector2::operator-(const Vector2& other) const {
+    return Vector2(x - other.x, y - other.y);
+}
+
+Vector2 Vector2::operator*(float scalar) const {
+    return Vector2(x * scalar, y * scalar);
+}
+
+Vector2 Vector2::operator/(float scalar) const {
+    return Vector2(x / scalar, y / scalar);
+}
+
+// Methods
+float Vector2::Length(const Vector2& v) {
+    return std::sqrt(v.x * v.x + v.y * v.y);
+}
+
+float Vector2::Distance(const Vector2& a, const Vector2& b) {
+    return Length(a - b);
+}
+
+float Vector2::Dot(const Vector2& a, const Vector2& b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+float Vector2::Cross(const Vector2& a, const Vector2& b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+Vector2 Vector2::Normalize() const {
+    float length = Length(*this);
+    return Vector2(x / length, y / length);
+}
+
+// Getters
+float Vector2::GetX() const {
+    return x;
+}
+
+float Vector2::GetY() const {
+    return y;
+}
+
+// Setters
+void Vector2::SetX(float x) {
+    this->x = x;
+}
+
+void Vector2::SetY(float y) {
+    this->y = y;
+}

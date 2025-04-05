88800c633cdc86f31817141ad20060d310b142ac
Liav Zidkiyaho <liavz2909@gmail.com>
2025-04-05 15:13:33 +0300

    Created test to Vector2 class in main

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "../Include/Vector2.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    Vector2 v1(3.0f, 4.0f);
+    Vector2 v2(1.0f, 2.0f);
+    Vector2 v3 = v1 + v2;
+    std::cout << "v3: (" << v3.GetX() << ", " << v3.GetY() << ")" << std::endl;
     return 0;
 }
